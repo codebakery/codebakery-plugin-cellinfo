15ab0c6530c282452616efc8b0a8fff60f4429f4
Dmitry Gladkov <dmitry.gladkov@gmail.com>
2015-09-18 17:08:28 +0300

    add platform check

--- www/cellinfo.js
+++ www/cellinfo.js
@@ -1,18 +1,21 @@
 var argscheck = require('cordova/argscheck');
 var exec = require('cordova/exec');
 
+if (platform.id === 'android') {
+    var CellInfo = function() {
+    };
 
-var CellInfo = function() {
-};
+    CellInfo.getNeighboringCellInfo = function(successCallback, errorCallback) {
+        argscheck.checkArgs('fF', 'CellInfo.getNeighboringCellInfo', arguments);
+        exec(successCallback, errorCallback, 'CellInfo', 'getNeighboringCellInfo', []);
+    };
 
-CellInfo.getNeighboringCellInfo = function(successCallback, errorCallback) {
-    argscheck.checkArgs('fF', 'CellInfo.getNeighboringCellInfo', arguments);
-    exec(successCallback, errorCallback, 'CellInfo', 'getNeighboringCellInfo', []);
-};
-
-CellInfo.getPrimaryCellInfo = function(successCallback, errorCallback) {
-    argscheck.checkArgs('fF', 'CellInfo.getPrimaryCellInfo', arguments);
-    exec(successCallback, errorCallback, 'CellInfo', 'getPrimaryCellInfo', []);
-};
+    CellInfo.getPrimaryCellInfo = function(successCallback, errorCallback) {
+        argscheck.checkArgs('fF', 'CellInfo.getPrimaryCellInfo', arguments);
+        exec(successCallback, errorCallback, 'CellInfo', 'getPrimaryCellInfo', []);
+    };
+} else {
+    var CellInfo = false;
+}
 
 module.exports = CellInfo;
